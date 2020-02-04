b40727c9c1f25cf7f7048b447eee354cc5c4fede
Steven Yan <steven.exe@outlook.com>
2020-02-04 18:50:42 +0800

    New

--- utils/log.js
+++
@@ -1,9 +0,0 @@
-function sayHello(name) {
-  console.log('hello ${name}!')
-}
-function sayBye(name) {
-  console.log('bye ${name}!')
-}
-
-module.exports.sayBye = sayHello
-module.exports.sayBye = sayBye
