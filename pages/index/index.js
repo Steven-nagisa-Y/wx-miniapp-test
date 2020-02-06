3a565ba408657d0a5b4744f5daf7c457426cc4dd
Steven Yan <steven_nagisa@qq.com>
2020-02-06 21:39:36 +0800

    v1.0.2

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,15 +5,117 @@ Page({
    * 页面的初始数据
    */
   data: {
-    "weatherImag": "/images/sunny.png",
-    "temper": "20˚C"
+    region: ["北京市", "北京市", "朝阳区"],
+    weatherImag: "/images/999.png",
+    temper: "-",
+    shiDu: "-",
+    qiYa: "-",
+    nengJianDu: "-",
+    fengSu: "-",
+    fengXiang: "-",
+    fengLi: "-",
+    updateTime: "",
+    code: "100"
   },
 
+  regionChanged: function(e) {
+    console.log(e.detail.value);
+    let regionTmp = e.detail.value;
+    this.setData({
+      region: regionTmp,
+    });
+    this.getWeather(0, 0);
+  },
+
+  getWeather: function(lat, lon) {
+    var that = this;
+    if (lat != 0 && lon != 0) {
+      var locationDetail = lat+','+lon;
+    } else {
+      var locationDetail = that.data.region[1];
+    }
+    console.log('Location Data: ' + locationDetail)
+    wx.request({
+      url: 'https://free-api.heweather.net/s6/weather/now?',
+      data: {
+        location: locationDetail,
+        key: "2ef71f1c0aec4e2a85927faf3a664b4c",
+      },
+      success: function(res) {
+        console.log(res.data);
+        console.log(res.data.HeWeather6[0].update.loc);
+        var nowWeather = res.data.HeWeather6[0].now;
+        if (res.data.HeWeather6[0].basic.parent_city == res.data.HeWeather6[0].basic.location) {
+          var regionDetail = [res.data.HeWeather6[0].basic.admin_area, res.data.HeWeather6[0].basic.parent_city];
+        } else {
+          var regionDetail = [res.data.HeWeather6[0].basic.admin_area, res.data.HeWeather6[0].basic.parent_city, res.data.HeWeather6[0].basic.location];
+        }
+        that.setData({
+          code: nowWeather.cond_code,
+          weatherImag: "/images/"+nowWeather.cond_code+".png",
+          temper: nowWeather.tmp+'˚C '+nowWeather.cond_txt,
+          shiDu: nowWeather.hum,
+          qiYa: nowWeather.pres / 10,
+          nengJianDu: nowWeather.vis,
+          fengSu: nowWeather.wind_spd,
+          fengXiang: nowWeather.wind_dir,
+          fengLi: nowWeather.wind_sc,
+          updateTime: "更新时间："+res.data.HeWeather6[0].update.loc,
+          region: regionDetail
+        })
+        that.changeColor(that.data.code);
+      },
+      fail: function(res) {
+        console.log(res.errMsg);
+        console.log(res.statusCode);
+      }
+    });
+  },
+
+  getLocation: function() {
+    var that = this;
+    wx.getLocation({
+      success: function(res) {
+        console.log(res)
+        var lat = res.latitude;
+        var lon = res.longitude;
+        that.getWeather(lat, lon);
+      },
+      fail: function(res) {
+        console.log(res);
+      }
+    })
+  },
+
+  changeColor: function(code) {
+    console.log("changeColor Code:" + code);
+    var that = this;
+    var now_color;
+    if (code == 100) {
+      now_color = '#66ccff';
+    } else if (code > 100 && code < 200) {
+      now_color = '#999999';
+    } else if (code >= 200 && code < 300) {
+      now_color = '#11ff11';
+    } else if (code >= 300 && code < 400) {
+      now_color = '#3300ff';
+    } else if (code >= 400 && code < 500) {
+      now_color = '#ffffff';
+    } else if (code >= 500 && code < 600) {
+      now_color = '#cc0000';
+    } else {
+      now_color = '#eeeeee';
+    }
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: now_color,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    this.getLocation();
   },
 
   /**
